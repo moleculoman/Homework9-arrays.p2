3395481e082f8f30e92407f149ad53d479009d7d
moleculoman <Xz2@ZBS@mhSFpg7>
2022-11-13 20:09:28 +0700

    hw9

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr = generateRandomArray();
+        System.out.println("Задача №1");
+        System.out.println("Сумма трат за месяц составила "+  arr[0] + " рублей");
+
+        System.out.println("Задача №2");
+        int max = arr[0];
+        int min = 100_000;
+        for (int i :arr){
+            if (i>= max) {
+                max = i;
+            } else if (i<min) {
+                min = i;
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила "+min+" рублей»");
+        System.out.println("Максимальная сумма трат за день составила "+max+" рублей»");
+
+
+
+
+
+
+
+
+
     }
+        public static int[] generateRandomArray() {
+            java.util.Random random = new java.util.Random();
+            int[] arr = new int[30];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = random.nextInt(100_000) + 100_000;
+            }
+            return arr;
+        }
 }
